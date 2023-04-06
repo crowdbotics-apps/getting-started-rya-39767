17f5d3dd159b5fef3120bc78fd2b78da441bd8ec
Ryan Brill <brill@crowdbotics.com>
2023-04-06 19:43:59 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,5 @@
+import { TextInput } from "react-native";
+import { Text } from "react-native";
 import { ImageBackground } from "react-native";
 import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
@@ -9,7 +11,8 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.bMBXxNGG}><ImageBackground style={styles.shxtnSCs} source={require("./Logo.png")} resizeMode="cover"></ImageBackground></View></ScrollView>
+    }}><View style={styles.bMBXxNGG}><ImageBackground style={styles.shxtnSCs} source={require("./Logo.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.PlqzcFjT} source={require("./Inputs.png")} resizeMode="cover"><TextInput style={styles.NOhETjbI} placeholder="Email" placeholderTextColor="#ffffff"></TextInput><TextInput style={styles.mIPARBqM} placeholder="Password" placeholderTextColor="#c0bbbb"></TextInput></ImageBackground><Text style={styles.pOICWndj}>Log In
+        </Text><ImageBackground style={styles.kHLIxtzy} source={require("./Time:Connection:Battery.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.doQtIkjU} source={require("./Home Indicator - On Light.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.tgIsyHyl} source={require("./Social Media Buttons.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.zvpUHcFt} source={require("./Buttons.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.rsrVwEOr} source={require("./Headline 2.png")} resizeMode="cover"></ImageBackground></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -30,7 +33,80 @@ const styles = StyleSheet.create({
   },
   shxtnSCs: {
     width: 87,
-    height: 87
+    height: 87,
+    position: "absolute",
+    top: 49,
+    left: 137
+  },
+  PlqzcFjT: {
+    width: 328,
+    height: 165,
+    position: "absolute",
+    top: 187,
+    left: 14
+  },
+  pOICWndj: {
+    width: 100,
+    height: 50,
+    lineHeight: 50,
+    fontSize: 14,
+    borderRadius: 0,
+    color: "#ffffff",
+    fontWeight: "700",
+    textAlign: "center",
+    position: "absolute",
+    top: 144,
+    left: 127
+  },
+  NOhETjbI: {
+    backgroundColor: "#2f3c50",
+    borderColor: "#cccccc",
+    width: 273,
+    height: 48,
+    position: "absolute",
+    top: 6,
+    left: 30
+  },
+  mIPARBqM: {
+    backgroundColor: "#2f3c50",
+    borderColor: "#cccccc",
+    width: 237,
+    height: 49,
+    position: "absolute",
+    top: 63,
+    left: 26
+  },
+  kHLIxtzy: {
+    width: 351,
+    height: 32
+  },
+  doQtIkjU: {
+    width: 107,
+    height: 37,
+    position: "absolute",
+    left: 126,
+    top: 531
+  },
+  tgIsyHyl: {
+    width: 187,
+    height: 67,
+    position: "absolute",
+    top: 425,
+    left: 80
+  },
+  zvpUHcFt: {
+    width: 280,
+    height: 62,
+    position: "absolute",
+    top: 362,
+    left: 39
+  },
+  rsrVwEOr: {
+    width: 168,
+    height: 21,
+    position: "absolute",
+    top: 509,
+    left: 95
   }
 });
 export default Untitled2;
--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,3 +1,4 @@
+import { ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +9,52 @@ const Untitled3 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><ImageBackground style={styles.wZmNafIs} source={require("./Home Screen.png")} resizeMode="cover"><ImageBackground style={styles.llFEmOLl} source={require("./Home Top Info.png")} resizeMode="cover"><ImageBackground style={styles.RBzaNanE} source={require("./Bottom Navigation.png")} resizeMode="cover"></ImageBackground></ImageBackground><ImageBackground style={styles.kOakMSyj} source={require("./Share and get.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.OjXpzcof} source={require("./History Section.png")} resizeMode="cover"></ImageBackground></ImageBackground></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  wZmNafIs: {
+    width: 354,
+    height: 576,
+    position: "absolute",
+    top: 0,
+    left: 0
+  },
+  llFEmOLl: {
+    width: 351,
+    height: 372
+  },
+  kOakMSyj: {
+    width: 315,
+    height: 103,
+    position: "absolute",
+    top: 383,
+    left: 18
+  },
+  OjXpzcof: {
+    width: 306,
+    height: 122,
+    position: "absolute",
+    top: 494,
+    left: 21
+  },
+  DZbvFyHF: {
+    width: 342,
+    height: 38,
+    position: "absolute",
+    top: 554,
+    left: -3
+  },
+  RBzaNanE: {
+    width: 339,
+    height: 60,
+    position: "absolute",
+    top: 507,
+    left: 21
   }
 });
 export default Untitled3;
